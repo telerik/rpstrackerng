30af2c44879893d0a85fa84d85ea50125c0dfa3e
Alex Ziskind <alex@nuvious.com>
2024-11-07 17:05:44 -0500

    re oved unused ngIf

--- src/app/modules/dashboard/components/active-issues/active-issues.component.ts
+++ src/app/modules/dashboard/components/active-issues/active-issues.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { StatusCounts } from '../../models';
-import { NgIf, DecimalPipe } from '@angular/common';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
     selector: 'app-active-issues',
     templateUrl: 'active-issues.component.html',
     styleUrls: ['active-issues.component.css'],
     standalone: true,
-    imports: [NgIf, DecimalPipe],
+    imports: [DecimalPipe],
 })
 export class ActiveIssuesComponent {
 
